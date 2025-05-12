bb3bd1f9fb66ed6256e045b1550a1f2a1cb19cc6
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-05-12 14:03:17 +0300

    Initial Commit

---
+++ AnimatedSplashScreen/AnimatedSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSplashScreenApp.swift
+//  AnimatedSplashScreen
+//
+//  Created by Vlad on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
